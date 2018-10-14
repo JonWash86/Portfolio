601b1db86acca8c8201d9592101c5d67f222afed
Jon Washington <jonathan.washington@gmail.com>
2018-10-14 10:16:30 -0700

    final commit before pull request

--- js/scripts.js
+++ js/scripts.js
@@ -18,5 +18,12 @@ function buildProjects(){
 
 
 function projectPop(){
-
+  for(var u = 0; u < projects.length; u++) {
+    var popup = document.getElementById('popup');
+    var popupInner = document.createElement('div');
+    var exitPopup = document.createElement('a');
+    exitPopup.setAttribute('class', 'exit-popup');
+    exitPopup.innerText = 'X';
+    exitPopup.setAttribute('onclick', 'location.relaod()');
+  }
 };
