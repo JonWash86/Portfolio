6598f024b0ba879e62685ab4838ea49b851c0ce8
Jon Washington <jonathan.washington@gmail.com>
2018-10-14 17:45:04 -0700

    began integrating jquery and functions per assignment

--- js/projects.js
+++ js/projects.js
@@ -1,9 +1,14 @@
+var projects = [];
+
 function project(title, thumbnail,){
   this.title = title;
   this.thumbnail = thumbnail;
 
-}
+};
 
-var projects = [];
+project.prototype.toHtml = function(){
+  var $newProject = $('article.template').clone();
+  
+};
 
 projects.push(new project('Cookie Stand', 'cookie_stand.jpg'));
