feeba241a63a48ff5e593fe0ef287a61f2b820bc
Jon Washington <jonathan.washington@gmail.com>
2018-10-14 17:56:10 -0700

    created rudimentary function to write project to DOM. Still buggy and imprecise.

--- js/projects.js
+++ js/projects.js
@@ -6,9 +6,18 @@ function project(title, thumbnail,){
 
 };
 
+
+
 project.prototype.toHtml = function(){
   var $newProject = $('article.template').clone();
-  
+  $newProject.removeClass('template');
+  $newProject.find('h1').text(this.title);
+  $newProject.append('<hr>');
+  return $newProject;
 };
 
 projects.push(new project('Cookie Stand', 'cookie_stand.jpg'));
+
+projects.forEach(function(project){
+  $('.main-content').append(project.toHtml());
+})
