fcc974a866fdefb0aac5be3a8fb9a435f28c7e63
Jon Washington <jonathan.washington@gmail.com>
2019-01-28 17:25:38 -0800

    minor changes as we dust this baby off

--- public/models/pages.js
+++ public/models/pages.js
@@ -7,10 +7,10 @@
   Page.all = [];
 
   Page.all.push(new Page(
-    'Projects', 'This is a page describing all of my projects. Clikc on one to find out more informations.'
+    'Projects', 'This is a page describing all of my projects. Click on one to find out more informations.'
   ));
   Page.all.push(new Page(
-    'About', 'This is the About page. So I guess you\'re reading about the about page right now.'
+    'About', 'This is the About page. Take a moment to bask in the recursion of reading about the about page.'
   ));
   Page.all.push(new Page(
     'Contact', 'This is how you can get in touch with me and say how much you love all my projects.'
@@ -24,17 +24,26 @@
 
   // Page.handleSidefo = function(){
     $('.tab').on('click', function(event){
+      $('.sidefoText').fadeOut();
       $('.sidefo').empty();
+      $('.sidefo').animate({
+        width:'toggle'
+      });
       var navTarget = $(this).attr('id');
       Page.all.forEach(function(page){
-        // console.log(this.pageTitle);
-        console.log(page);
         if (navTarget == page.pageTitle){
           $('.sidefo').append(page.toFo());
-        };
-      });
+          $('.sidefoText').animate({
+            opacity: 100
+          })
+        $('.sidefo').animate({
+          width:'toggle'
+        });
+
+      //     // $('.sidefo').toggle('slide').delay(6000);
+      }
     });
-  // };
+  });
 
   // $(document).ready(function() {
   //   Page.handleSidefo();
